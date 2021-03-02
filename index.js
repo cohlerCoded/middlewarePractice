d1db81a9866dee254891bc2ec28c7c2d67cb6985
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-02 15:41:30 -0800

    etc

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
-app.use(morgan("tiny"));
+//app.use(morgan("tiny"));
 app.use((req, res, next) => {
-  console.log("THIS IS MY FIRST MIDDLEWARE!");
-  next();
-});
-app.use((req, res, next) => {
-  console.log("THIS IS MY SECOND MIDDLEWARE!");
+  req.requestTime = Date().toString();
+  console.log(req.method.toLocaleUpperCase(), req.path);
   next();
 });
 
+// app.use((req, res, next) => {
+//   console.log("THIS IS MY FIRST MIDDLEWARE!");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("THIS IS MY SECOND MIDDLEWARE!");
+//   next();
+// });
+
 app.get("/", (req, res) => {
+  console.log(`Request Date: ${req.requestTime}`);
   res.send("home page!!!");
 });
 

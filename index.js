9966fc2167f1e6b1e76e25c1b3133f22e61ff00c
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-02 14:42:04 -0800

    defining middleware

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(morgan("tiny"));
+app.use((req, res, next) => {
+  console.log("THIS IS MY FIRST MIDDLEWARE!");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("THIS IS MY SECOND MIDDLEWARE!");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("home page!!!");
